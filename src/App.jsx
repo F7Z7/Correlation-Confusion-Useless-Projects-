0a264e8017ce8b81dc1b75be843b5a829369e2bc
farza <farzanrichu610@gmail.com>
2025-08-01 23:52:59 +0530

    set up LetterGlitch.jsx from react bits

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,26 @@ import {useState} from 'react'
 
 import './App.css'
 import Hero from "./Pages/Hero.jsx";
+import LetterGlitch from "./components/LetterGlitch.jsx";
 
 function App() {
     const [count, setCount] = useState(0)
 
     return (
-        <>
+        <div className="relative min-h-screen w-full overflow-hidden">
+            {/* Background */}
+            <div className="absolute inset-0 -z-10">
+                <LetterGlitch
+                    glitchSpeed={50}
+                    centerVignette={true}
+                    outerVignette={false}
+                    smooth={true}
+                />
+            </div>
+
+
             <Hero/>
-        </>
+        </div>
     )
 }
 
