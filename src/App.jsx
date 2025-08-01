afd234fa51cff3829250ad322b68d70fe677cc03
farza <farzanrichu610@gmail.com>
2025-08-01 19:04:29 +0530

    initialised page structures

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Hero from "./Pages/Hero.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
       <>
-          <h1 className="text-3xl text-red-500 italic underline ">
-              Hello world!
-          </h1>
+<Hero/>
       </>
   )
 }
---
+++ src/Pages/Hero.jsx
@@ -0,0 +1,14 @@
+//hero page of the site
+import React from 'react';
+
+
+
+function Hero() {
+    return (
+        <div className="flex flex-col bg-emerald h-screen justify-center items-center ">
+            <p className="text-red-500 font-bold text-xl">HEy this is the entry point</p>
+        </div>
+    );
+}
+
+export default Hero;
