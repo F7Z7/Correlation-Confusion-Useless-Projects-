ce63c4aca83ce6b47ff3ff44cf57f374975d7a72
farza <farzanrichu610@gmail.com>
2025-08-01 19:31:11 +0530

    removed hover

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -5,7 +5,7 @@ import SubHeadings from "../components/SubHeadings.jsx";
 function Hero() {
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-violet-600 via-violet-600 to-indigo-600  p-6">
-            <div className="w-full max-w-2xl text-center bg-white/20 backdrop-blur-lg rounded-3xl shadow-2xl p-8 transform transition-all duration-500 hover:scale-105">
+            <div className="w-full max-w-2xl text-center bg-white/20 backdrop-blur-lg rounded-3xl shadow-2xl p-8 ">
                 <Header title="Correlation Confusion"/>
                 <SubHeadings text="Lets the find the relationship btw something that doesn't have any relationship."/>
 
