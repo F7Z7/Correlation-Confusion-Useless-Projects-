2bb7c269f9770bd75ff13debef93254cd447b458
farza <farzanrichu610@gmail.com>
2025-08-01 23:28:48 +0530

    set up fixed width for the button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Button({text, onClick,icon: Icon,gradient}) {
     return (
         <div>
-            <button onClick={onClick} className={`flex p-3 text-white items-center gap-2 border rounded-xl cursor-pointer hover:scale-105 hover:shadow-lg transition-all duration-300 ${gradient}`}>
+            <button onClick={onClick} className={`flex p-3 text-white text-center justify-center items-center gap-2 border rounded-xl cursor-pointer hover:scale-105 hover:shadow-lg transition-all duration-300 w-[170px] ${gradient}`}>
                 {Icon && <Icon className="w-5 h-5" />}
                 {text}
             </button>
